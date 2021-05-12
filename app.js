de6457d3b5d4e4cc1e3e8bbaad6c1a659c57d38a
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-05-12 17:10:50 -0600

    feature/potion

---
+++ app.js
@@ -0,0 +1,7 @@
+// TDD is test-driven development
+// stages of TDD are: red green refactor 
+// jest is a testing framework for node.js (created by facebook)
+//  jest is showing as deprecated??
+// change the scripts in package.json to "jest"
+
+// __tests__ is the directory / *.test.js is the file naming convention
